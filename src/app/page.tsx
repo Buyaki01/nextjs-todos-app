5530e9c8bc686f64ca5126d4f09e30bedccdb2e0
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-28 09:24:51 +0300

    Add AddTodo, Navbar, Todo, TodoList component and lib/fetchTodo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import TodoList from "./components/TodoList"
+import AddTodo from "./components/AddTodo"
 
-export const revalidate = 0 
+export const revalidate = 0
 
 export default function Home() {
+
   return (
-    <> 
+    <>
       <AddTodo />
-      {/* @ts-expect-error Server Component */}
       <TodoList />
     </>
   )
