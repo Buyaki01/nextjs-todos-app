0438fb2f6760312a571a27328976702331801f58
Buyaki01 <rittahbuyaki@gmail.com>
2023-09-28 08:33:23 +0300

    Add lib/fetchTodos to fetch todos from the backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+
+export const revalidate = 0 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
-    </main>
+    <> 
+      <AddTodo />
+      {/* @ts-expect-error Server Component */}
+      <TodoList />
+    </>
   )
 }
